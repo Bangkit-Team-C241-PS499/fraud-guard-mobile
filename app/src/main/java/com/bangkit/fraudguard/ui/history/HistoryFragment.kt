af4d768b2b23a66a490e15f5e9444af322941f40
Glan Harith Teguh <glangulen@gmail.com>
2024-06-11 21:48:58 +0700

    feat(history-list): implement history page

--- app/src/main/java/com/bangkit/fraudguard/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/bangkit/fraudguard/ui/history/HistoryFragment.kt
@@ -1,60 +1,80 @@
 package com.bangkit.fraudguard.ui.history
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bangkit.fraudguard.R
+import com.bangkit.fraudguard.data.adapters.HistoryAdapter
+import com.bangkit.fraudguard.data.dto.response.History
+import com.bangkit.fraudguard.databinding.FragmentHistoryBinding
+import com.bangkit.fraudguard.databinding.FragmentHomeBinding
+import com.bangkit.fraudguard.ui.main.MainViewModel
+import com.bangkit.fraudguard.ui.viewModelFactory.ViewModelFactory
+import com.bangkit.fraudguard.ui.welcome.WelcomeActivity
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HistoryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HistoryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentHistoryBinding? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private val binding get() = _binding!!
+    private lateinit var viewModel: MainViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false)
+        _binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        setupViewModel()
+        checkUserSession()
+        binding.rvHistoryPage.layoutManager = LinearLayoutManager(requireActivity())
+        binding.rvHistoryPage.addItemDecoration(
+            DividerItemDecoration(
+                requireContext(),
+                LinearLayoutManager(requireActivity()).orientation
+            )
+        )
+        showHistory()
+        return root
+
+    }
+    private fun setupViewModel() {
+
+        viewModel = ViewModelProvider(
+            requireActivity(), ViewModelFactory.getInstance(requireContext())
+        ).get(MainViewModel::class.java)
+    }
+    private fun checkUserSession() {
+        viewModel.getSession().observe(viewLifecycleOwner, Observer { userModel ->
+            if (!userModel.isLogin) {
+                startActivity(Intent(requireActivity(), WelcomeActivity::class.java))
+                requireActivity().finish()
+            }
+        })
+    }
+    private fun showHistory() {
+        viewModel.getHistory().observe(viewLifecycleOwner, Observer { response ->
+            if (response.isSuccessful) {
+                val historyList: List<History>? = response.body()
+                if (historyList != null) {
+                    val adapter = HistoryAdapter()
+                    adapter.submitList(historyList)
+                    binding.rvHistoryPage.adapter = adapter
+
+                } else {
+                    Log.e("HOMEFRAGMENT", "History list is null")
+                }
+            } else {
+                Log.e("HOMEFRAGMENT", "Failed to load history: ${response.errorBody()?.string()}")
+            }
+        })
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HistoryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HistoryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
