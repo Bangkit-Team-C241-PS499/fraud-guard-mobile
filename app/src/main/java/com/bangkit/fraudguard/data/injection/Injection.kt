09233ceaa118140bbd3706d5794c471917ade63a
gabiiing13 <gabiiing13@gmail.com>
2024-06-13 16:00:38 +0700

    feat: develop article list in article page and home page

--- app/src/main/java/com/bangkit/fraudguard/data/injection/Injection.kt
+++ app/src/main/java/com/bangkit/fraudguard/data/injection/Injection.kt
@@ -1,9 +1,11 @@
 package com.bangkit.fraudguard.data.injection
 
 import android.content.Context
+import com.bangkit.fraudguard.data.config.getApiServiceArticle
 import com.bangkit.fraudguard.data.config.getApiServiceAuth
 import com.bangkit.fraudguard.data.preferences.UserPreference
 import com.bangkit.fraudguard.data.preferences.dataStore
+import com.bangkit.fraudguard.data.repository.ArticleRepository
 import com.bangkit.fraudguard.data.repository.AuthRepository
 import com.bangkit.fraudguard.data.repository.SpamRepository
 
@@ -16,5 +18,10 @@ object Injection {
     fun provideSpamRepositoryInjection(context: Context): SpamRepository {
         val pref = UserPreference.getInstance(context.dataStore)
         return SpamRepository.getInstance(pref)
+    }
+
+    fun provideArticleRepositoryInjection(context: Context): ArticleRepository {
+        val apiServiceArticle = getApiServiceArticle()
+        return ArticleRepository.getInstance(apiServiceArticle)
     }
 }
