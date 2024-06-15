c6219591c5a9876713021b475baa40a359ea0b57
Glan Harith Teguh <glangulen@gmail.com>
2024-06-15 21:46:13 +0700

    feat(create-prediction):implemented

---
+++ app/src/main/java/com/bangkit/fraudguard/data/dto/response/PredictResponse.kt
@@ -0,0 +1,27 @@
+package com.bangkit.fraudguard.data.dto.response
+
+import com.google.gson.annotations.SerializedName
+
+data class PredictResponse(
+
+	@field:SerializedName("createdAt")
+	val createdAt: String? = null,
+
+	@field:SerializedName("UserId")
+	val userId: Int? = null,
+
+	@field:SerializedName("prediction")
+	val prediction: Any? = null,
+
+	@field:SerializedName("id")
+	val id: String? = null,
+
+	@field:SerializedName("label")
+	val label: String? = null,
+
+	@field:SerializedName("message")
+	val message: String? = null,
+
+	@field:SerializedName("updatedAt")
+	val updatedAt: String? = null
+)
